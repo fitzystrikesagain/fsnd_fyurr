389a87fbab6fac1c07d53797a5969413387beb5d
Fitzy <fitzystrikesagain@gmail.com>
2021-06-13 15:41:09 -0500

    finish building out model columns

--- models.py
+++ models.py
@@ -6,42 +6,54 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Venue(db.Model):
-    __tablename__ = "venue"
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-
 class Artist(db.Model):
-    __tablename__ = "artist"
+    __tablename__ = "artists"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.String(128))
+    city = db.Column(db.String(128))
+    state = db.Column(db.String(128))
+    phone = db.Column(db.String(128))
+    facebook_link = db.Column(db.String(128))
+    seeking_venue = db.Column(db.Boolean, default=False)
+    image_link = db.Column(db.String(512))
+    website = db.Column(db.String(128))
+    seeking_description = db.Column(db.String)
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    def __repr__(self):
+        return f"<Artist id: {self.id}; name: {self.name}>"
 
 
 class Shows(db.Model):
     __tablename__ = "shows"
+
     venue_id = db.Column(db.Integer, primary_key=True)
     venue_name = db.Column(db.String)
     artist_id = db.Column(db.Integer)
     artist_name = db.Column(db.String)
     artist_image_link = db.Column(db.String)
     start_time = db.Column(db.String)
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
+    def __repr__(self):
+        return f"<Show id: {self.id}; name: {self.name}>"
+
+
+class Venue(db.Model):
+    __tablename__ = "venues"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    genres = db.Column(db.String)
+    address = db.Column(db.String(128))
+    city = db.Column(db.String(128))
+    state = db.Column(db.String(128))
+    phone = db.Column(db.String(128))
+    website = db.Column(db.String)
+    facebook_link = db.Column(db.String(128))
+    seeking_talent = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String)
+    image_link = db.Column(db.String(512))
+
+    def __repr__(self):
+        return f"<Venue id: {self.id}; name: {self.name}>"
