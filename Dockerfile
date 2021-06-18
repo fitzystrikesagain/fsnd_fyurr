4e278faf6524d3eb0fc94be93f671ed3d6457066
Fitzy <fitzystrikesagain@gmail.com>
2021-06-17 21:46:10 -0500

    get rid of node build and clean up script

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,4 @@
-FROM node:alpine
-WORKDIR '/app'
-COPY package.json .
-RUN npm install
-COPY . .
-RUN npm run build
-
 FROM python:3
-
 WORKDIR /usr/src/app
-
-COPY requirements.txt ./
+COPY ./requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
--- startup.sh
+++ startup.sh
@@ -15,10 +15,9 @@ EOF
 # Clean up all containers, fresh build
 
 
-# Wait for Flask, open website, exec into container
+# Load env vars and start container
+step "1" "Start" "Waiting for $FLASK_CONTAINER_NAME to start"
 source .env
-mkdir -p data
-step "1" "Begin" "Waiting for $FLASK_CONTAINER_NAME to start"
 docker compose up -d
 while [[ "$(curl -s -o /dev/null -w "%{http_code}" localhost:"$FLASK_PORT")" != "200" ]]; do
   printf .
@@ -26,21 +25,18 @@ while [[ "$(curl -s -o /dev/null -w "%{http_code}" localhost:"$FLASK_PORT")" != 
 done
 step "1" "End" "Waiting for $FLASK_CONTAINER_NAME to start"
 
-step "2" "Begin" "Initializing database and performing migrations"
+step "2" "Start" "Initializing database and performing migrations"
 docker exec -ti fyyur flask db init
 docker exec -ti fyyur flask db migrate
 docker exec -ti fyyur flask db upgrade
 step "2" "End" "Initializing database and performing migrations"
 
-step "3" "Begin" "Launching website"
-open http://localhost:"$FLASK_PORT"
-step "3" "End" "Launching website"
-
-step "4" "Begin" "Loading tables with venues, artists, and shows data"
+step "3" "Start" "Loading tables with venues, artists, and shows data"
 docker exec -u root "$POSTGRES_CONTAINER_NAME" psql "$POSTGRES_DB" "$POSTGRES_USER" -f /tmp/venues.sql
 docker exec -u root "$POSTGRES_CONTAINER_NAME" psql "$POSTGRES_DB" "$POSTGRES_USER" -f /tmp/artists.sql
 docker exec -u root "$POSTGRES_CONTAINER_NAME" psql "$POSTGRES_DB" "$POSTGRES_USER" -f /tmp/shows.sql
-step "4" "End" "Loading tables with venues, artists, and shows data"
+step "3" "End" "Loading tables with venues, artists, and shows data"
 
-step "5" "Launching pgcli"
-pgcli "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/$POSTGRES_DB"
+step "4" "Start" "Launching website"
+open http://localhost:"$FLASK_PORT"
+step "4" "End" "Launching website"
